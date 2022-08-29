8b174c1cffe88e6fd00c6dbbdfcbecc803177934
Shubham <shubham.gupta@woodside.com.au>
2022-08-29 19:50:23 +0800

    Dynamic Routing and acquiring userId in req.params

--- routes/users.js
+++ routes/users.js
@@ -12,4 +12,13 @@ router.get("/new", (req, res) => {
 	res.send("New User Form");
 });
 
+// Now you can also create Dynamic Routes like this and access it via req.params
+// Important thing to remember with static and dynamic routes is that Express goes top-down.
+// So if anyhting from top to bottom matches, it returns that.
+// So anything Static should always be on top of Dynamic Routes.
+// If the /:userId dynamic route is put above /new, users/new will route to users/:userId since userId is just a variable.
+router.get("/:userId", (req, res) => {
+	res.send(`Get User with ID: ${req.params.userId}`);
+});
+
 module.exports = router;
