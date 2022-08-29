506583f6d0756851c13bb18c31a2be50ae3959a1
Shubham <shubham.gupta@woodside.com.au>
2022-08-29 19:27:21 +0800

    basic EJS instalation and usage

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { red } = require("@mui/material/colors");
 const express = require("express");
 
 const app = express();
@@ -5,9 +6,22 @@ const app = express();
 // listen on port 3000
 app.listen(3000);
 
-
 // define your routes
-app.get('/', (req,res) => [
-    // run code when you're trying to acess '/'
-    res.send("Hello world")
-])
+app.get("/", (req, res) => [
+	// run this code when you're trying to acess '/'
+
+	// Send Basic Message.
+	// res.send("Hello world")
+
+	// Send JSON
+	// res.json({ message: "Hi" })
+
+	// Send a file for user to download
+	// res.download('server.js')
+
+	// render HTML from a file caled index.html
+	res.render("index", { text: "World" }),
+]);
+
+// Set your View Engine as EJS.
+app.set("view engine", "ejs");
